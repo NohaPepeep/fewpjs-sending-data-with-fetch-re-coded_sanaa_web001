782b8e60e13f74408c12f2a0d3c11c59a8f63e95
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-20 11:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(user,email){
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name : user,
+    email : email
+  })
+})
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object[1])
+  })
+.catch(function(error) {
+  let a =document.createTextNode(error.message);
+    document.body.appendChild(a);
+  })
+}
+//document.body.innerHTML
+submitData("nn","sss")
